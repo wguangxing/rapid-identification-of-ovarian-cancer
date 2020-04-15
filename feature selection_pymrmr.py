ff56d4c1aa727e293cff343271bd8e02d5143171
wguangxing <39231388+wguangxing@users.noreply.github.com>
2020-04-16 00:12:54 +0800

    Add files via upload

---
+++ feature selection_pymrmr.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+import pymrmr
+
+df = pd.read_csv() #input the feature dataset that needs to be processed
+
+pymrmr.mRMR( , , ) #pymrmr.mRMR(param1, 'param2', param3), param1:feature dataset, param2:MIQ or MID, param3:an integer which defines the number of features that should be selected by the algorithm.
